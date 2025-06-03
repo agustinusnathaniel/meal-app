779570f7bcc3b00a2ece3c7e26bfd535d307a438
Agustinus Nathaniel <berserk.nate@gmail.com>
2025-06-03 20:23:39 +0700

    chore: use vite-tsconfig-paths

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,20 @@
 import { defineConfig } from "vite";
 import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
-import { resolve } from "node:path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), viteReact(), tailwindcss()],
+  plugins: [
+    TanStackRouterVite({ autoCodeSplitting: true }),
+    viteReact(),
+    tailwindcss(),
+    tsConfigPaths(),
+  ],
   test: {
     globals: true,
     environment: "jsdom",
   },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-    },
-  }
 });
