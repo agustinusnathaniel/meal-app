796158a0051fc67de7721db48f865a1be85ca3dc
Agustinus Nathaniel <berserk.nate@gmail.com>
2025-06-03 21:07:34 +0700

    refactor: define head meta

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,14 +1,80 @@
 import type { QueryClient } from '@tanstack/react-query';
-import { createRootRouteWithContext, Outlet } from '@tanstack/react-router';
+import {
+  createRootRouteWithContext,
+  HeadContent,
+  Outlet,
+} from '@tanstack/react-router';
 import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 
 import { Providers } from '@/components/providers';
 import { RootLayout } from '@/layout';
 
+const title = 'Meal App';
+const description = 'Meal database';
+const url = 'https://zippy.sznm.dev';
+const ogImgUrl =
+  'https://og.sznm.dev/api/generate?heading=meal-app&text=Meal+Database&template=color';
+
 export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
   {
+    head: () => ({
+      meta: [
+        {
+          title: title,
+        },
+        {
+          name: 'theme-color',
+          content: '#000000',
+        },
+        {
+          name: 'description',
+          content: description,
+        },
+        {
+          name: 'og:type',
+          content: 'website',
+        },
+        {
+          name: 'og:url',
+          content: url,
+        },
+        {
+          name: 'og:title',
+          content: title,
+        },
+        {
+          name: 'og:description',
+          content: description,
+        },
+        {
+          name: 'og:image',
+          content: ogImgUrl,
+        },
+        {
+          name: 'twitter:card',
+          content: 'summary_large_image',
+        },
+        {
+          name: 'twitter:url',
+          content: url,
+        },
+        {
+          name: 'twitter:title',
+          content: title,
+        },
+        {
+          name: 'twitter:description',
+          content: description,
+        },
+        {
+          name: 'twitter:image',
+          content: ogImgUrl,
+        },
+      ],
+    }),
     component: () => (
       <Providers>
+        <HeadContent />
         <RootLayout>
           <Outlet />
         </RootLayout>
