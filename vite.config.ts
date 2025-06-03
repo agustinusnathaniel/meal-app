814564250969f123303233e0271cc14a5f9e12dd
Agustinus Nathaniel <berserk.nate@gmail.com>
2025-06-03 21:09:53 +0700

    chore: configure vite pwa

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,27 @@ import viteReact from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import tsConfigPaths from 'vite-tsconfig-paths';
 import checker from 'vite-plugin-checker'
+import { VitePWA, type VitePWAOptions } from 'vite-plugin-pwa';
+
+const pwaOptions: Partial<VitePWAOptions> = {
+  registerType: 'autoUpdate',
+  base: '/',
+  manifest: {
+    name: 'Meal App',
+    short_name: 'meal-app',
+    theme_color: '#000000',
+    lang: 'en',
+    start_url: '/',
+    background_color: '#FFFFFF',
+    dir: 'ltr',
+    display: 'standalone',
+    prefer_related_applications: false,
+  },
+  pwaAssets: {
+    disabled: false,
+    config: true,
+  },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,7 +38,8 @@ export default defineConfig({
     checker({
       typescript: true,
       biome: true,
-    })
+    }),
+    VitePWA(pwaOptions),
   ],
   server: {
     open: true
