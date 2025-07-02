a52179d54d1a38585776091b5ba68ea38930c814
Agustinus Nathaniel <berserk.nate@gmail.com>
2025-07-02 12:03:23 +0700

    chore: update packages to latest

--- src/routeTree.gen.ts
+++ src/routeTree.gen.ts
@@ -8,65 +8,34 @@
 // You should NOT make any changes in this file as it will be overwritten.
 // Additionally, you should also exclude this file from your linter and/or formatter to prevent it from being checked or modified.
 
-// Import Routes
+import { Route as rootRouteImport } from './routes/__root'
+import { Route as IndexRouteImport } from './routes/index'
+import { Route as MealViewMealIdRouteImport } from './routes/meal/view/$mealId'
 
-import { Route as rootRoute } from './routes/__root'
-import { Route as IndexImport } from './routes/index'
-import { Route as MealViewMealIdImport } from './routes/meal/view/$mealId'
-
-// Create/Update Routes
-
-const IndexRoute = IndexImport.update({
+const IndexRoute = IndexRouteImport.update({
   id: '/',
   path: '/',
-  getParentRoute: () => rootRoute,
+  getParentRoute: () => rootRouteImport,
 } as any)
-
-const MealViewMealIdRoute = MealViewMealIdImport.update({
+const MealViewMealIdRoute = MealViewMealIdRouteImport.update({
   id: '/meal/view/$mealId',
   path: '/meal/view/$mealId',
-  getParentRoute: () => rootRoute,
+  getParentRoute: () => rootRouteImport,
 } as any)
-
-// Populate the FileRoutesByPath interface
-
-declare module '@tanstack/react-router' {
-  interface FileRoutesByPath {
-    '/': {
-      id: '/'
-      path: '/'
-      fullPath: '/'
-      preLoaderRoute: typeof IndexImport
-      parentRoute: typeof rootRoute
-    }
-    '/meal/view/$mealId': {
-      id: '/meal/view/$mealId'
-      path: '/meal/view/$mealId'
-      fullPath: '/meal/view/$mealId'
-      preLoaderRoute: typeof MealViewMealIdImport
-      parentRoute: typeof rootRoute
-    }
-  }
-}
-
-// Create and export the route tree
 
 export interface FileRoutesByFullPath {
   '/': typeof IndexRoute
   '/meal/view/$mealId': typeof MealViewMealIdRoute
 }
-
 export interface FileRoutesByTo {
   '/': typeof IndexRoute
   '/meal/view/$mealId': typeof MealViewMealIdRoute
 }
-
 export interface FileRoutesById {
-  __root__: typeof rootRoute
+  __root__: typeof rootRouteImport
   '/': typeof IndexRoute
   '/meal/view/$mealId': typeof MealViewMealIdRoute
 }
-
 export interface FileRouteTypes {
   fileRoutesByFullPath: FileRoutesByFullPath
   fullPaths: '/' | '/meal/view/$mealId'
@@ -75,37 +44,34 @@ export interface FileRouteTypes {
   id: '__root__' | '/' | '/meal/view/$mealId'
   fileRoutesById: FileRoutesById
 }
-
 export interface RootRouteChildren {
   IndexRoute: typeof IndexRoute
   MealViewMealIdRoute: typeof MealViewMealIdRoute
+}
+
+declare module '@tanstack/react-router' {
+  interface FileRoutesByPath {
+    '/': {
+      id: '/'
+      path: '/'
+      fullPath: '/'
+      preLoaderRoute: typeof IndexRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+    '/meal/view/$mealId': {
+      id: '/meal/view/$mealId'
+      path: '/meal/view/$mealId'
+      fullPath: '/meal/view/$mealId'
+      preLoaderRoute: typeof MealViewMealIdRouteImport
+      parentRoute: typeof rootRouteImport
+    }
+  }
 }
 
 const rootRouteChildren: RootRouteChildren = {
   IndexRoute: IndexRoute,
   MealViewMealIdRoute: MealViewMealIdRoute,
 }
-
-export const routeTree = rootRoute
+export const routeTree = rootRouteImport
   ._addFileChildren(rootRouteChildren)
   ._addFileTypes<FileRouteTypes>()
-
-/* ROUTE_MANIFEST_START
-{
-  "routes": {
-    "__root__": {
-      "filePath": "__root.tsx",
-      "children": [
-        "/",
-        "/meal/view/$mealId"
-      ]
-    },
-    "/": {
-      "filePath": "index.tsx"
-    },
-    "/meal/view/$mealId": {
-      "filePath": "meal/view/$mealId.tsx"
-    }
-  }
-}
-ROUTE_MANIFEST_END */
